193297be092aac61f245f8c99150e9a7e4192fcf
Davis <davis.sermons@gmail.com>
2018-05-24 23:05:16 +0300

    feat: restart button in modal

--- js/app.js
+++ js/app.js
@@ -1,21 +1,20 @@
-//Create a list that holds all of your cards
-const iconsList = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
+/*
+ * Global variables
+ */
 
+const iconsList = ["fa fa-diamond", "fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
 const cardsContainer = document.querySelector (".deck");
+const modal = document.querySelector(".modal");
 
 let openedCards = [];
 let matchedCards = [];
 let firstClick = true;
-
-//Timer variables
 let currentTimer = 0;
 let interval = 0;
 let lastUpdateTime = new Date().getTime();
 let mins = document.querySelector('span.minutes');
 let secs = document.querySelector('span.seconds');
 let cents = document.querySelector('span.centiseconds');
-
-//Modal variables
 let starCount = 3;
 
 /*
@@ -23,19 +22,16 @@ let starCount = 3;
  */
 
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
 }
-
 
 /*
  * Create cards to start the game
@@ -56,10 +52,10 @@ function createCards() {
   }
 }
 
-
 /*
  * Click event
  */
+
 function click(card) {
   //Card click event
   card.addEventListener("click", function(){
@@ -68,10 +64,10 @@ function click(card) {
     const previousCard = openedCards[0];
 
     // Only the first click calls timer function
-        if(firstClick) {
-            startTimer();
-            firstClick = false;
-        }
+    if(firstClick) {
+      startTimer();
+      firstClick = false;
+    }
 
     //Have existing opened card
     if(openedCards.length === 1) {
@@ -82,14 +78,13 @@ function click(card) {
       //Compare 2 cards
       compare(currentCard, previousCard);
 
-
     } else {
 
-    //Don't have any opened cards
-    currentCard.classList.add("open", "show", "disable");
-    openedCards.push(this);
-  }
-});
+      //Don't have any opened cards
+      currentCard.classList.add("open", "show", "disable");
+      openedCards.push(this);
+    }
+  });
 }
 
 /*
@@ -120,6 +115,7 @@ function click(card) {
 
      openedCards = [];
    }
+
    //Add move
    addMove();
  }
@@ -158,106 +154,132 @@ function gameOver() {
  /*
  * Game Over Message
  */
+
  function modalPop() {
 
-   const modal = document.querySelector(".modal");
+   // Display the modal
+   modal.style.top = "0";
 
+   // Add moves to the Modal
+   const totalMoves = document.querySelector("#totalMoves");
+   totalMoves.innerHTML = moves;
 
-     // Display the modal
-     modal.style.top = "0";
+   // Add Rate
+   const addRate = document.querySelector("#totalRate");
+   totalRate.innerHTML = starCount;
 
-     // Add moves to the Modal
-     const totalMoves = document.querySelector("#totalMoves");
-     totalMoves.innerHTML = moves;
+   // Stop Timer
+   stopTimer();
 
-     // Add Rate
-     const addRate = document.querySelector("#totalRate");
-     totalRate.innerHTML = starCount;
-
-     // Stop Timer
-     stopTimer();
-
-    // Add time to the Modal
-    const totalMinutes = document.querySelector("#totalMinutes");
-    const totalSeconds = document.querySelector("#totalSeconds");
-    const totalMiliseconds = document.querySelector("#totalMiliseconds");
-    totalMinutes.innerHTML = mins.innerHTML;
-    totalSeconds.innerHTML = secs.innerHTML;
-    totalMiliseconds.innerHTML = cents.innerHTML;
-   }
-
+   // Add time to the Modal
+   const totalMinutes = document.querySelector("#totalMinutes");
+   const totalSeconds = document.querySelector("#totalSeconds");
+   const totalMiliseconds = document.querySelector("#totalMiliseconds");
+   totalMinutes.innerHTML = mins.innerHTML;
+   totalSeconds.innerHTML = secs.innerHTML;
+   totalMiliseconds.innerHTML = cents.innerHTML;
+ }
 
 /*
  * Raiting system
  */
- const starsContainer = document.querySelector(".stars");
-const star = `<li><i class="fa fa-star"></i></li>`;
-starsContainer.innerHTML = star + star + star;
-function rating() {
 
-    if( moves < 15) {
-        starsContainer.innerHTML = star + star + star;
-        starCount = 3;
-    } else if( moves < 20) {
-        starsContainer.innerHTML = star + star;
-        starCount = 2;
-    } else {
-        starsContainer.innerHTML = star;
-        starCount = 1;
-    }
-}
+ const starsContainer = document.querySelector(".stars");
+ const star = `<li><i class="fa fa-star"></i></li>`;
+ starsContainer.innerHTML = star + star + star;
+ function rating() {
+
+   if( moves < 15) {
+     starsContainer.innerHTML = star + star + star;
+     starCount = 3;
+   } else if( moves < 20) {
+     starsContainer.innerHTML = star + star;
+     starCount = 2;
+   } else {
+     starsContainer.innerHTML = star;
+     starCount = 1;
+   }
+ }
 
 /*
  * Timer function
  */
 
-//Function returns 2 digit, for example: 1 = 01
-function twoDigit (n) {
-  return ("00" + n).substr(-2);
-}
+ //Function returns 2 digit, for example: 1 = 01
+ function twoDigit (n) {
+   return ("00" + n).substr(-2);
+ }
 
-//Updates the time
-function update () {
-  let now = new Date().getTime(),
-  dt = now - lastUpdateTime; //delta time
+ //Updates the time
+ function update () {
+   let now = new Date().getTime(),
 
-  currentTimer += dt;
+   //Delta time calculates time
+   dt = now - lastUpdateTime;
+   currentTimer += dt;
 
-  let time = new Date(currentTimer);
+   let time = new Date(currentTimer);
 
-  mins.innerHTML = twoDigit(time.getMinutes());
-  secs.innerHTML = twoDigit(time.getSeconds());
-  cents.innerHTML = twoDigit(Math.floor(time.getMilliseconds() / 10));
+   mins.innerHTML = twoDigit(time.getMinutes());
+   secs.innerHTML = twoDigit(time.getSeconds());
+   cents.innerHTML = twoDigit(Math.floor(time.getMilliseconds() / 10));
 
-  lastUpdateTime = now;
-}
+   lastUpdateTime = now;
+ }
 
 /*
  * Start and reset timer functions
  */
-function startTimer() {
-  if (!interval) {
-    lastUpdateTime = new Date().getTime();
-    interval = setInterval(update, 1);
-  }
-}
 
-  function stopTimer() {
-    clearInterval(interval);
-    interval = 0;
-  }
+ function startTimer() {
+   if (!interval) {
+     lastUpdateTime = new Date().getTime();
+     interval = setInterval(update, 1);
+   }
+ }
 
-  function resetTimer() {
-    stopTimer();
-    currentTimer = 0;
-    mins.innerHTML = secs.innerHTML = cents.innerHTML = twoDigit(0)
-  }
+ function stopTimer() {
+   clearInterval(interval);
+   interval = 0;
+ }
+
+ function resetTimer() {
+   stopTimer();
+   currentTimer = 0;
+   mins.innerHTML = secs.innerHTML = cents.innerHTML = twoDigit(0)
+ }
 
 /*
  * Restart the game
  */
-const restartBtn = document.querySelector(".restart");
-restartBtn.addEventListener("click", function() {
+
+ const restartBtn = document.querySelector(".restart");
+ restartBtn.addEventListener("click", function() {
+
+   //Delete cards
+   cardsContainer.innerHTML = "";
+
+   //Call 'createCards' function to start new game
+   createCards();
+
+   //Reset any related variables
+   matchedCards = [];
+   moves = 0;
+   movesContainer.innerHTML = moves;
+   starsContainer.innerHTML = star + star + star;
+   firstClick = true;
+   resetTimer();
+ })
+
+/*
+ * Restart the game in modal
+ */
+
+const restartBtnModal = document.querySelector(".play-again");
+restartBtnModal.addEventListener("click", function() {
+
+  // Hide the modal
+  modal.style.top = "-200%";
 
   //Delete cards
   cardsContainer.innerHTML = "";
@@ -272,26 +294,10 @@ restartBtn.addEventListener("click", function() {
   starsContainer.innerHTML = star + star + star;
   firstClick = true;
   resetTimer();
-
-
-
 })
 
-
-
-
-
-// Create cards to start the game
-createCards();
-
-
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ * Create cards to start the game!
  */
+
+createCards();
